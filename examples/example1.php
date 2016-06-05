61deb087c5765c58fc45d42c5c1b0c73e8498381
mhthnz <and-rochev@ya.ru>
2016-06-05 19:37:59 +0500

    add example1

---
+++ examples/example1.php
@@ -0,0 +1,40 @@
+<?php
+include "../SteamPlayer.php";
+
+// Must set SteamApi key
+SteamPlayer::$API_KEY = 'STEAM API KEY HERE';
+
+// Find player by identifier
+$steamID = '76561198088033749';
+$player = SteamPlayer::Create($steamID);
+
+// Show data
+echo 'steam id '.$player->steamID()."<br/>\r\n";
+echo 'nickname: '.$player->nickName()."<br/>\r\n";
+echo 'realname: '.$player->realName()."<br/>\r\n";
+echo 'countrycode: '.$player->countryCode()."<br/>\r\n";
+echo 'localitycode: '.$player->localityCode()."<br/>\r\n";
+echo 'avatarsmall: <img src="'.$player->avatar(SteamPlayer::AVATAR_SMALL).'">'."<br/>\r\n";
+echo 'avatarmedium: <img src="'.$player->avatar(SteamPlayer::AVATAR_MEDIUM).'">'."<br/>\r\n";
+echo 'countrycode: <img src="'.$player->avatar(SteamPlayer::AVATAR_LARGE).'">'."<br/>\r\n";
+echo 'private profile: '.($player->isPrivate() ? 'true' : 'false')."<br/>\r\n";
+echo 'is playing: '. $player->isPlaying()."<br/>\r\n";
+echo 'game name: '. $player->gameName()."<br/>\r\n";
+echo 'game id: '. $player->gameId()."<br/>\r\n";
+
+// Save avatar
+$player->saveAvatar('small.jpg', SteamPlayer::AVATAR_SMALL);
+$player->saveAvatar('medium.jpg', SteamPlayer::AVATAR_MEDIUM);
+$player->saveAvatar('large.jpg', SteamPlayer::AVATAR_LARGE);
+
+// Get original object
+echo '<hr/>Original Object: <br/>';
+var_dump($player->get());
+echo "<hr/><br/>\r\n";
+
+
+// Get other properties via magic method
+echo 'lastlogoff: '.$player->lastlogoff."<br/>\r\n";
+echo 'loccityid: '.$player->loccityid."<br/>\r\n";
+echo 'primaryclanid: '.$player->primaryclanid."<br/>\r\n";
+echo 'timecreated: '.$player->timecreated."<br/>\r\n";
